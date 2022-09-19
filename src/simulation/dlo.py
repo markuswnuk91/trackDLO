09561eb2384f6b4abcff4edc16f32056fdc6d4a0
Markus Wnuk <markus.wnuk@isw.uni-stuttgart.de>
2022-09-19 18:35:33 +0200

    worked on class DLO

---
+++ src/simulation/dlo.py
@@ -0,0 +1,261 @@
+import os, sys
+import numpy as np
+import math
+import dartpy as dart
+
+
+class DeformableLinearObject:
+    """
+    Class for representing a Deformable Linear Object (DLO) in Dynamics and Animation Robotics Toolbox (DART).
+
+    Attributes:
+    skel (dart.dynamcis.Skeleton): Skeleton Representation as a Dart Skelton (refer to Dart's skeleton class doucmentation for futher details)
+    frames(dict of list of dart.dynamics.SimpleFrame): SimpleFrame can be attached to the DLO. The DLO stores simple frames in a dict, where each entry contains a list of dart.dynamics.SimpleFrame.
+    length (float): Length of the DLO
+    radius (float): Radius of the DLO
+    density (float): Density of the DLO
+    name (str): Name of the skeleton. If None name is generated automatically.
+    stiffness(float): stiffness of the DLO.
+    dampint(float): daming of the DLO.
+    color (np.array): Color of the DLO in RGB values, e.g. [0,0,1] for blue.
+    gravity (bool): If the DLO should be affected by gravity. If None defaults to true.
+    collidable (bool): If the DLO is collidable. If None defaults to true.
+    """
+
+    ID = 0
+
+    def __init__(
+        self,
+        numSegments: int = 20,
+        length: float = 1,
+        radius: float = 0.01,
+        density: float = 1000,
+        name: str = None,
+        stiffness: float = 1,
+        damping: float = 0.1,
+        color: np.array = np.array([0, 0, 1]),
+        gravity: bool = True,
+        collidable: bool = True,
+        adjacentBodyCheck: bool = False,
+        enableSelfCollisionCheck: bool = True,
+    ):
+
+        self.ID = DeformableLinearObject.ID
+        DeformableLinearObject.ID += 1
+        if name is None:
+            self.name = "DLO_" + str(self.ID)
+            self.skel = dart.dynamics.Skeleton(name="DLO_" + str(self.ID))
+        else:
+            self.name = name
+            self.skel = dart.dynamics.Skeleton(name=self.name)
+
+        self.numSegments = numSegments
+        self.length = length
+        self.radius = radius
+        self.density = density
+        self.stiffness = stiffness
+        self.damping = damping
+        self.color = color
+        self.segmentLength = self.length / self.numSegments
+        self.adjacentBodyCheck = adjacentBodyCheck
+        self.enableSelfCollisionCheck = enableSelfCollisionCheck
+        self.frames = {}
+
+        if gravity is None:
+            self.gravityMode = True
+        else:
+            self.gravity = gravity
+
+        if collidable is None:
+            self.collidable = True
+        else:
+            self.collidable = collidable
+
+        self.makeRootBody()
+
+        for i in range(self.numSegments - 1):
+            self.addBody(self.skel.getBodyNodes()[-1])
+            i += 1
+
+        # disable adjacent body collision check by default
+        self.skel.setAdjacentBodyCheck(self.adjacentBodyCheck)
+
+        # enable selfCollisionChecking by default
+        if self.enableSelfCollisionCheck:
+            self.skel.enableSelfCollisionCheck()
+        else:
+            self.skel.disableSelfCollisionCheck()
+
+        print("Succesfully created Skeleton: " + self.name)
+
+    def makeRootBody(self):
+        # rootJoint properties
+        rootjoint_prop = dart.dynamics.FreeJointProperties()
+        rootjoint_prop.mName = self.name + "_root" + "_joint"
+        rootjoint_prop.mRestPositions = np.zeros(6)
+        rootjoint_prop.mSpringStiffnesses = np.zeros(6)
+        rootjoint_prop.mDampingCoefficients = np.zeros(6)
+
+        # rootbody properties
+        rootbody_aspect_prop = dart.dynamics.BodyNodeAspectProperties(
+            name=self.name + "_root_body"
+        )
+        rootbody_prop = dart.dynamics.BodyNodeProperties(rootbody_aspect_prop)
+
+        # create joint&bodyNode pair
+        [rootjoint, rootbody] = self.skel.createFreeJointAndBodyNodePair(
+            None, rootjoint_prop, rootbody_prop
+        )
+        rootbody.setGravityMode(self.gravity)
+        rootbody.setCollidable(self.collidable)
+
+        # set shapes
+        self.setBodyShape_Cylinder(
+            rootbody,
+            radius=self.radius,
+            length=(self.segmentLength - 2 * self.radius),
+            color=self.color,
+        )
+
+        # set the transformation between rootjoint and rootbody
+        tf = dart.math.Isometry3()
+        bodyNodeCenter = [0, 0, -self.segmentLength / 2.0]
+        tf.set_translation(bodyNodeCenter)
+        rootjoint.setTransformFromChildBodyNode(tf)
+
+        self.setJointShape_Ball(body=rootbody, radius=self.radius)
+
+    def addBody(self, parentNode, name=None, offset=0.0):
+        joint_prop = dart.dynamics.BallJointProperties()
+        if name is None:
+            joint_prop.mName = (
+                self.name + "_" + str(self.skel.getNumBodyNodes()) + "_joint"
+            )
+        else:
+            joint_prop.mName = name + "_joint"
+
+        joint_prop.mRestPositions = np.zeros(3)
+        joint_prop.mSpringStiffnesses = np.ones(3) * self.stiffness
+        joint_prop.mDampingCoefficients = np.ones(3) * self.damping
+        joint_prop.mT_ParentBodyToJoint.set_translation(
+            [0, 0, self.segmentLength / 2.0 + offset]
+        )
+        if name is None:
+            body_aspect_prop = dart.dynamics.BodyNodeAspectProperties(
+                name=(self.name + "_" + str(self.skel.getNumBodyNodes()) + "_body")
+            )
+        else:
+            body_aspect_prop = dart.dynamics.BodyNodeAspectProperties(name=name)
+
+        body_prop = dart.dynamics.BodyNodeProperties(body_aspect_prop)
+        [joint, body] = self.skel.createBallJointAndBodyNodePair(
+            parentNode, joint_prop, body_prop
+        )
+        body.setGravityMode(self.gravity)
+        body.setCollidable(self.collidable)
+
+        self.setBodyShape_Cylinder(
+            body,
+            radius=self.radius,
+            length=(self.segmentLength - 2 * self.radius),
+            color=self.color,
+        )
+        # set the transformation between parent joint and body
+        tf = dart.math.Isometry3()
+        bodyNodeCenter = [0, 0, -self.segmentLength / 2.0]
+        tf.set_translation(bodyNodeCenter)
+        joint.setTransformFromChildBodyNode(tf)
+
+        self.setJointShape_Ball(body=body, radius=self.radius)
+
+    def setJointShape_Ball(self, body, radius, color=[0, 0, 1]):
+        ballShape = dart.dynamics.SphereShape(radius)
+        shape_node = body.createShapeNode(ballShape)
+        visual = shape_node.createVisualAspect()
+        visual.setColor(color)
+        tf = dart.math.Isometry3()
+        jointCenter = [0, 0, -self.segmentLength / 2.0]
+        tf.set_translation(jointCenter)
+        shape_node.setRelativeTransform(tf)
+
+    def setBodyShape_Cylinder(self, body, radius, length, color=[0, 0, 1]):
+        cylinderShape = dart.dynamics.CylinderShape(radius, length)
+        shape_node = body.createShapeNode(cylinderShape)
+        visual = shape_node.createVisualAspect()
+        shape_node.createCollisionAspect()
+        shape_node.createDynamicsAspect()
+        visual.setColor(color)
+
+        # set mass and inertia of the body
+        inertia = cylinderShape.computeInertiaFromDensity(self.density)
+        volume = cylinderShape.getVolume()
+        inertia_dart = dart.dynamics.Inertia()
+        inertia_dart.setMoment(inertia)
+        body.setInertia(inertia_dart)
+        body.setMass(volume * self.density)
+
+    def getNumSegments(self):
+        return self.skel.getNumBodyNodes()
+
+    def setInitialPosition(self, dof: int, q: float):
+        """
+        Set the initial position for a degree of freedom of the DLO.
+        Also sets the rest position for this degree of freedom to the given intial position.
+
+        Args:
+            dof (int): degree of freedom for which the initial position should be set
+            q (float): initial position for this degree of freedom.
+        """
+        self.skel.getDof(dof).setPosition(q)
+        self.skel.getDof(dof).setRestPosition(self.skel.getDof(dof).getPosition())
+
+    def getRestPosition(self, dof: int, q: float):
+        return self.skel.getDof(dof).getRestPosition()
+
+    def getPosition(self, dof: int):
+        return self.skel.getDof(dof).getPosition()
+
+    def addSimpleFrame(
+        self,
+        key: str,
+        bodyNodeNumber: int,
+        relTransform: np.ndarray = np.eye(4),
+        name: str = None,
+        shape: dart.dynamics.Shape = None,
+        shapeColor: np.array = np.array([0, 0, 1]),
+    ):
+        """
+        Adds a simple frame to this DLO. Simple frames are grouped by keywords.
+        For each key the DLO holds a list of dart.dynamics.SimpleFrames
+
+        Args:
+            key (str): The key specifying the group for the simple frame to add, e.g.
+            "Marker" or "Target".
+            bodyNodeNumber (int): The index number of the body node the simple frame should be attached to.
+            relTransform (dart.math.Isometry3): the relativeTransformation in the local coordinate system of the body node.
+        """
+        # check if key already exists
+        if key in self.frames:
+            frameList = self.frames[key]
+        else:
+            frameList = []
+
+        if name is None:
+            frameName = self.name + "_" + key + "_" + str(len(frameList))
+        else:
+            frameName = name
+        tf = dart.math.Isometry3()
+        tf.set_translation(relTransform[:3, 3])
+        tf.set_rotation(relTransform[:3, :3])
+        newFrame = dart.dynamics.SimpleFrame(
+            self.skel.getBodyNode(bodyNodeNumber), frameName, relTransform
+        )
+
+        # set the shape if given
+        if shape is not None:
+            newFrame.setShape(shape)
+            visualAspect = newFrame.createVisualAspect()
+            visualAspect.setColor(shapeColor)
+
+        frameList.append(newFrame)
+        self.frames[key] = frameList
---
+++ tests/dlo_test.py
@@ -0,0 +1,157 @@
+# tests dart installation
+import os, sys
+import dartpy as dart
+import numpy as np
+import math
+import time as time
+from pytest import approx
+
+try:
+    sys.path.append(os.getcwd().replace("/tests", ""))
+    from src.simulation.dlo import DeformableLinearObject
+except:
+    print("Imports for DLO failed.")
+    raise
+
+visualize = True
+
+
+def test_initDLO():
+    world = dart.simulation.World()
+
+    # test constructor from numSegments
+    testDLO = DeformableLinearObject(13)
+    assert testDLO.getNumSegments() == 13
+
+    # test default constructor
+    testDLO = DeformableLinearObject()
+    assert testDLO.getNumSegments() == 20
+    assert testDLO.density == 1000
+    assert testDLO.radius == 0.01
+    assert testDLO.length == 1
+    assert testDLO.segmentLength == 1 / 20
+    assert testDLO.stiffness == 1
+    assert testDLO.damping == 0.1
+    assert (testDLO.color == np.array([0, 0, 1])).all()
+    assert testDLO.gravity == True
+    assert testDLO.collidable == True
+    assert testDLO.adjacentBodyCheck == False
+    assert testDLO.enableSelfCollisionCheck == True
+    assert (
+        testDLO.skel.getBodyNode(0).getWorldTransform().translation()
+        == np.array([0, 0, testDLO.segmentLength / 2])
+    ).all()
+    assert np.linalg.norm(
+        testDLO.skel.getBodyNode(testDLO.skel.getNumBodyNodes() - 1)
+        .getWorldTransform()
+        .translation()
+        - np.array([0, 0, testDLO.length - testDLO.segmentLength / 2])
+    ) == approx(0, abs=1e-12)
+
+    # test setInitialPosition
+    testDLO.setInitialPosition(0, 2.0)
+    assert testDLO.skel.getDof(0).getPosition() == 2.0
+    assert testDLO.skel.getDof(0).getRestPosition() == 2.0
+    assert testDLO.getPosition(0) == 2.0
+
+    testDLO.setInitialPosition(1, 2.0)
+    assert testDLO.skel.getDof(1).getPosition() == 2.0
+    assert testDLO.skel.getDof(1).getRestPosition() == 2.0
+    assert testDLO.getPosition(1) == 2.0
+
+    testDLO.setInitialPosition(2, 2.0)
+    assert testDLO.skel.getDof(2).getPosition() == 2.0
+    assert testDLO.skel.getDof(2).getRestPosition() == 2.0
+    assert testDLO.getPosition(2) == 2.0
+
+    testDLO.setInitialPosition(3, 0.1)
+    assert testDLO.skel.getDof(3).getPosition() == 0.1
+    assert testDLO.skel.getDof(3).getRestPosition() == 0.1
+    assert testDLO.getPosition(3) == 0.1
+
+    testDLO.setInitialPosition(4, 0.1)
+    assert testDLO.skel.getDof(4).getPosition() == 0.1
+    assert testDLO.skel.getDof(4).getRestPosition() == 0.1
+    assert testDLO.getPosition(4) == 0.1
+
+    testDLO.setInitialPosition(5, 0.1)
+    assert testDLO.skel.getDof(5).getPosition() == 0.1
+    assert testDLO.skel.getDof(5).getRestPosition() == 0.1
+    assert testDLO.getPosition(5) == 0.1
+
+    # test masss and inertia values
+    for i in range(testDLO.getNumSegments()):
+        assert (
+            testDLO.skel.getBodyNode(i).getInertia().getMass()
+            == dart.dynamics.CylinderShape(
+                testDLO.radius, testDLO.segmentLength - 2 * testDLO.radius
+            ).getVolume()
+            * testDLO.density
+        )
+        assert (
+            testDLO.skel.getBodyNode(i).getInertia().getMoment()
+            == dart.dynamics.CylinderShape(
+                testDLO.radius, testDLO.segmentLength - 2 * testDLO.radius
+            ).computeInertiaFromDensity(testDLO.density)
+        ).all()
+
+    # test adding a simple frame
+    testDLO.addSimpleFrame(
+        key="Target",
+        bodyNodeNumber=0,
+    )
+    testTransform = np.array(
+        ([1, 0, 0, 0.1], [0, 1, 0, 0.2], [0, 0, 1, 0.3], [0, 0, 0, 1])
+    )
+    testDLO.addSimpleFrame(
+        key="Testframes",
+        bodyNodeNumber=3,
+        name="myTestFrame",
+        relTransform=testTransform,
+        shape=dart.dynamics.BoxShape(3 * testDLO.radius * np.ones(3)),
+        shapeColor=np.array([1, 0, 0]),
+    )
+
+    testDLO.addSimpleFrame(
+        key="Testframes",
+        bodyNodeNumber=10,
+        name="myTestFrame",
+        shape=dart.dynamics.BoxShape(3 * testDLO.radius * np.ones(3)),
+        shapeColor=np.array([1, 0, 0]),
+    )
+
+    # test simulation
+    world.addSkeleton(testDLO.skel)
+    node = dart.gui.osg.RealTimeWorldNode(world)
+    try:
+        world.step()
+        stepError = False
+    except:
+        stepError = True
+
+    assert not stepError
+
+    if visualize:
+        try:
+            # Create world node and add it to viewer
+            viewer = dart.gui.osg.Viewer()
+            viewer.addWorldNode(node)
+
+            # Grid settings
+            grid = dart.gui.osg.GridVisual()
+            grid.setPlaneType(dart.gui.osg.GridVisual.PlaneType.XY)
+            grid.setOffset([0, 0, 0])
+            viewer.addAttachment(grid)
+
+            viewer.setUpViewInWindow(0, 0, 1200, 900)
+            viewer.setCameraHomePosition([8.0, 8.0, 4.0], [0, 0, -2.5], [0, 0, 1])
+            viewer.run()
+            visError = False
+        except:
+            visError = True
+
+        assert not visError
+
+
+if __name__ == "__main__":
+    test_initDLO()

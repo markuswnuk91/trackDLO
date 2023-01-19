fixed costFun of Discrete Reconstruction to match Jacobian Calculation

plot/reconstructHelix.py
@@ -24,7 +24,7 @@ except:
     raise
 
 saveParams = False
-plotSteps = True
+plotSteps = False
 plotFinal = True
 
 # continuous reconstruction
@@ -36,7 +36,7 @@ loadPathInitialParameters = "/mnt/c/Users/ac129490/Documents/Dissertation/Softwa
 # discrete reconsruction
 reconstructDiscrete = True
 numIterDiscrete = 300
-numSegments = 6
+numSegments = 5
 
 savePath = "plot/plotdata/helixReconstruction/"
 fileName_continuousParams = "helix_continuousModel"
@@ -125,7 +125,7 @@ if __name__ == "__main__":
     s = np.linspace(0, 1, 30)
     Y = helixCurve(s)
 
-    sDiscrete = np.linspace(0, 1, numSegments)
+    sDiscrete = np.linspace(0, 1, numSegments + 1)
     discreteY = helixCurve(sDiscrete)
 
     # reconstruct with contiuous model
@@ -168,9 +168,10 @@ if __name__ == "__main__":
             }
         )
 
-    correspondanceWeightingFactor = np.ones(numSegments)
-    # correspondanceWeightingFactor[:4] = 5
-    # correspondanceWeightingFactor[-1] = 5
+    correspondanceWeightingFactor = np.ones(numSegments + 1)
+    correspondanceWeightingFactor[2] = 100
+
+    # correspondanceWeightingFactor[-1] = 1
     discreteReconstruction = DiscreteReconstruction(
         **{
             "Y": discreteY,

src/modelling/finiteSegmentModel.py
@@ -17,9 +17,9 @@ except:
 class FiniteSegmentModel(DeformableLinearObject):
     """Implementation of a finite segment representation of a DLO.
     The implementation is based on the theoretic principles of Discrete Kirchoff Rods from the paper:
-    "Miklos Bergou et al., Discrete Elastic Rods, ACM Transactions on Graphics (SIGGRAPH), 2008"
-    and uses the Dynamics Animation and Robotics Toolkit (DART) to model the underlying kinematics.
-    The class contains the functionality to map between different representations and allows to convert from the continous space (local coordinate s) to the discrete space (indices of segments i).
+    "Miklos Bergou et al., Discrete Elastic Rods, ACM Transactions on Graphics (SIGGRAPH), 2008".
+    It uses the Dynamics Animation and Robotics Toolkit (DART) to model the underlying kinematics.
+    The class contains the functionality to map between the different representations and allows to convert from the continous space (local coordinate s) to the discrete space (segments i).
 
     Attributes
         ----------
@@ -53,13 +53,11 @@ class FiniteSegmentModel(DeformableLinearObject):
         q: np.array
             Degrees of freedom of the dart representation
 
+        x0: np.array
+            1x3 position of the beginning of the first segment
+
         rot0: np.array
             1x3 angles to transform from the reference coordinate system to the first segment
-
-        jointAngles: np.array
-            Nx3 angles of the Dart balljoints degrees of freedom. Rotations (alpha, beta, gamma) around the segment coordinate axes
-    Args:
-        object (_type_): _description_
     """
 
     def __init__(
@@ -125,9 +123,6 @@ class FiniteSegmentModel(DeformableLinearObject):
             *args,
             **kwargs,
         )
-        # for i, jointAngles in enumerate(self.angles):
-        #     for jointAngle in jointAngles:
-        #         self.setPosition(i, jointAngle)
 
     def mapDartPositionsToAngles(self, q):
         x0 = q[3:6]

src/reconstruction/discreteReconstruction.py
@@ -147,12 +147,10 @@ class DiscreteReconstruction(ShapeReconstruction, FiniteSegmentModel):
             # self.annealingFlex**self.iter * self.evalUflex(self.L, self.numSc)
             # + self.annealingTor**self.iter * self.evalUtor(self.L, self.numSc)
             # + self.evalUgrav(self.L, self.numSc)
-            +self.wPosDiff
-            * np.square(
-                np.sum(
-                    self.correspondanceWeightingFactor
-                    * np.linalg.norm((self.Y - self.X), axis=1)
-                )
+            self.wPosDiff
+            * np.sum(
+                self.correspondanceWeightingFactor
+                * np.square(np.linalg.norm(self.Y - self.X, axis=1))
             )
             # + self.wPosDiff * (1 - np.exp(-np.linalg.norm(self.Y - self.X) / 1000))
             # + np.sum(self.aPsi)
@@ -176,17 +174,18 @@ class DiscreteReconstruction(ShapeReconstruction, FiniteSegmentModel):
             for jacobian in jacobians:
                 # fill dart jacobians with zeros
                 paddedJacobian = np.pad(
-                    jacobian.copy(),
+                    jacobian,
                     ((0, 0), (0, len(self.q) - jacobian.shape[1] % len(self.q))),
                     "constant",
                 )
                 jacobianRows.append(paddedJacobian[3:6, correspondingDartIndex])
             jacobianMultiplikatorMatrix = np.vstack(jacobianRows)
             # calcualte cost function derivative
-            J[i] = (
+            J[i] = np.sum(
                 -2
                 * self.wPosDiff
-                * np.sum((self.Y - self.X) * jacobianMultiplikatorMatrix)
+                * self.correspondanceWeightingFactor
+                * np.sum((self.Y - self.X) * jacobianMultiplikatorMatrix, axis=1)
             )
 
         return J
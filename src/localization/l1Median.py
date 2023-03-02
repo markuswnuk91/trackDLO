5ccbdfd9cbea3985d8a7ddf61436e6a36748daed
Markus Wnuk <markus.wnuk@isw.uni-stuttgart.de>
2023-03-02 14:52:13 +0100

    added L1-Median implementation and some fixes

---
+++ src/localization/l1Median.py
@@ -0,0 +1,198 @@
+import numpy as np
+from scipy.spatial import distance_matrix
+import numbers
+from warnings import warn
+
+
+class L1Median(object):
+    """
+    Implementation according to the Paper
+    "Huang et al.: L1-Medial Skeleton of Point Cloud, ACM Transactions on Graphics, 32(4):1, 2013"
+    INPUT:
+        Q:      Jx3 np.ndarray, pointCloud the skeleton line should be extracted from
+        X:      Ix3 np.ndarray, seedpoints used to represent the sought centerline
+        h:      real value, support radius h defining the size of the supporting local neighborhood for L1-medial skeleton
+        mu:     real value, weighting parameter for repulsion force between seedpoints
+        iterations:   int, number of iterations to perfrom for aligning seedpoints
+    """
+
+    def __init__(self, Q, X, h=None, mu=0.35, iterations=100):
+        if type(Q) is not np.ndarray or Q.ndim != 2:
+            raise ValueError("The source point set (Q) must be at a 2D numpy array.")
+        if type(X) is not np.ndarray or X.ndim != 2:
+            raise ValueError("The seed point set (X) must be at a 2D numpy array.")
+        if Q.shape[1] != X.shape[1]:
+            raise ValueError(
+                "Both point sets need to have the same number of dimensions."
+            )
+        if Q.shape[0] < Q.shape[1] or X.shape[0] < X.shape[1]:
+            raise ValueError(
+                "The dimensionality is larger than the number of points. Possibly the wrong orientation of Q and X."
+            )
+        if iterations is not None and (
+            not isinstance(iterations, numbers.Number) or iterations < 0
+        ):
+            raise ValueError(
+                "Expected a positive integer for iterations instead got: {}".format(
+                    iterations
+                )
+            )
+
+        self.X = X
+        self.X0 = X
+        self.Q = Q
+        self.h = 0.12 if h is None else h
+        self.mu = 0.35 if mu is None else mu
+        self.iterations = 100 if iterations is None else 100
+        self.iter = 0
+
+    def get_h0(self, points):
+
+        x_max = points[:, 0].max()
+        x_min = points[:, 0].min()
+
+        y_max = points[:, 1].max()
+        y_min = points[:, 1].min()
+
+        z_max = points[:, 2].max()
+        z_min = points[:, 2].min()
+
+        diagonal = (
+            (x_max - x_min) ** 2 + (y_max - y_min) ** 2 + (z_max - z_min) ** 2
+        ) ** 0.5
+
+        Npoints = len(points)
+
+        return 2 * diagonal / (Npoints ** (1.0 / 3))
+
+    def get_thetas(self, r, h):
+        """
+        INPUT:
+            r: NxM np.ndarray of distances given by a distance matrix of two datasets NxK and MxK, where N and M are number of points and K are dimensions
+            h: support radius h defining the size of the supporting local neighborhood for L1-medial skeleton
+
+        OUTPUT:
+            thetas: NXM np.ndarray of weighting factors for each point correspondence
+        """
+        thetas = np.exp((-(r**2)) / ((h / 2) ** 2))
+        # Clip to JUST not zero
+        # thetas =  np.clip(thetas, 10**-323, None)
+        return thetas
+
+    def get_alphas(self, X, Q, h):
+        """
+        INPUT:
+            Q:      Jx3 np.ndarray, pointCloud
+            X:      Ix3 np.ndarray, seedpoints to represent the sought centerline
+            h: support radius h defining the size of the supporting local neighborhood for L1-medial skeleton
+        """
+        distances = distance_matrix(X, Q)
+        thetas = self.get_thetas(distances, h)
+        alphas = np.divide(
+            thetas,
+            distances,
+            out=np.zeros(thetas.shape, dtype=float),
+            where=distances != 0,
+        )
+        return alphas
+
+    def get_betas(self, X, h):
+        """
+        INPUT:
+            X:      Ix3 np.ndarray, seedpoints to represent the sought centerline
+            h: support radius h defining the size of the supporting local neighborhood for L1-medial skeleton
+        """
+        distances = distance_matrix(X, X)
+        thetas = self.get_thetas(distances, h)
+        distances2 = distances * distances
+        betas = np.divide(
+            thetas,
+            distances2,
+            out=np.zeros(thetas.shape, dtype=float),
+            where=distances2 != 0,
+        )
+        return betas
+
+    def get_sigmas(self, X, h):
+        """
+        INPUT:
+            X:      Ix3 np.ndarray, seedpoints to represent the sought centerline
+            h: support radius h defining the size of the supporting local neighborhood for L1-medial skeleton
+
+        OUTPUT:
+            sigmas: Ix1 vector of sigmas
+        """
+        thetas = self.get_thetas(distance_matrix(X, X), h)
+        sigmas = np.zeros(len(X))
+        for i, x in enumerate(X):
+            C_i = np.zeros((3, 3))
+            for i_dash, x_dash in enumerate(np.delete(X, i, 0)):
+                C_i += thetas[i, i_dash] * np.outer(x, x_dash)
+            lambdas, eigVecs = np.linalg.eig(C_i)
+
+            if np.iscomplex(lambdas).any():
+                # print("Found complex eigenvalues.")
+                lambdas = np.real(lambdas)
+            sigmas[i] = np.amax(lambdas) / np.sum(lambdas)
+        return sigmas
+
+    def calculateL1Median(self):
+        """
+        Function to perform L1 Median estimation.
+        """
+        # ---------------------------------#
+        # begin algorithm
+        # ---------------------------------#
+        J = len(self.Q)  # number of input points
+        I = len(self.X)  # number of seedpoints
+
+        alpha_matrix = np.zeros((I, J))
+        beta_matrix = np.zeros((I, I))
+
+        while self.iter < self.iterations:
+            alpha_matrix = self.get_alphas(self.X, self.Q, self.h)
+            beta_matrix = self.get_betas(self.X, self.h)
+            sigmas = self.get_sigmas(self.X, self.h)
+            sum_J_qj_aij = np.column_stack(
+                (
+                    # sum over x dimension
+                    np.sum(alpha_matrix * self.Q[:, 0].transpose(), axis=1),
+                    # sum over y dimension
+                    np.sum(alpha_matrix * self.Q[:, 1].transpose(), axis=1),
+                    # sum over z dimension
+                    np.sum(alpha_matrix * self.Q[:, 2].transpose(), axis=1),
+                )
+            )
+            sum_J_aij = np.sum(alpha_matrix, axis=1)
+            term1 = sum_J_qj_aij / sum_J_aij[:, None]  # mean shift term
+
+            sum_Id_xixid_betaid = np.sum(
+                (
+                    # repeat points along first dimenstion to make cube of I x I x 3 and subtract transposed Ix3xI cube
+                    np.tile(self.X, (I, 1, 1))
+                    - np.transpose(np.tile(self.X, (I, 1, 1)), (1, 0, 2))
+                )
+                # multiply with beta factor along first dimension
+                * np.transpose(np.tile(beta_matrix, (3, 1, 1)), (2, 1, 0))
+                # sum over first dimension to obtain agian Ix3 array
+                ,
+                axis=0,
+            )
+            sum_Id_betaiid = np.sum(beta_matrix, axis=1) - np.diag(beta_matrix)
+            term2 = (
+                self.mu
+                * sigmas[:, None]
+                * (sum_Id_xixid_betaid / sum_Id_betaiid[:, None])
+            )  # regularization term
+
+            # update positions
+            self.X = term1 + term2
+            self.iter += 1
+
+            if callable(self.callback):
+                self.callback()
+
+        return self.X
+
+    def registerCallback(self, callback):
+        self.callback = callback

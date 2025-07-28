added fix for DART Jacobian: use first body instead of world

src/tracking/kpr/kinematicsModel.py
@@ -22,52 +22,96 @@ class KinematicsModelDart(object):
             X[n, :] = self.skel.getBodyNode(n).getWorldTransform().translation()
         return X
 
-    def getJacobian(self, q, n):
+    def getJacobian(self, q, n, method=None):
         """
         Placeholder for child classes
         """
         self.skel.setPositions(q)
-        # J = np.zeros((3, self.Dof))
+        if method is None:
+            # J = np.zeros((3, self.Dof))
 
-        # dartJacobian = self.skel.getBodyNode(n).getWorldJacobian(np.array([0, 0, 0]))[
-        #     3:6, :
-        # ]
-        # # dartJacobian[:, 3:6] = (
-        # #     np.linalg.inv(self.skel.getBodyNode(0).getWorldTransform().rotation())
-        # #     @ dartJacobian[:, 3:6]
-        # # )
+            # dartJacobian = self.skel.getBodyNode(n).getWorldJacobian(np.array([0, 0, 0]))[
+            #     3:6, :
+            # ]
+            # # dartJacobian[:, 3:6] = (
+            # #     np.linalg.inv(self.skel.getBodyNode(0).getWorldTransform().rotation())
+            # #     @ dartJacobian[:, 3:6]
+            # # )
 
-        # # (
-        # #     np.linalg.inv(self.skel.getBodyNode(0).getWorldTransform().rotation())
-        # #     @ self.skel.getBodyNode(n).getWorldJacobian(np.array([0, 0, 0]))[3:6, :]
-        # # )
-        # if dartJacobian.shape[1] < self.Dof:
-        #     #     J = np.pad(
-        #     #         dartJacobian,
-        #     #         ((0, 0), (0, self.Dof - dartJacobian.shape[1] % self.Dof)),
-        #     #         "constant",
-        #     #     )
-        #     indexPointer = 0
-        #     paddedJacobian = np.zeros((3, self.skel.getNumDofs()))
-        #     for i in range(0, self.skel.getNumDofs()):
-        #         if self.skel.getBodyNode(n).dependsOn(i):
-        #             paddedJacobian[:, i] = dartJacobian[:, indexPointer]
-        #             indexPointer += 1
-        #     J = paddedJacobian
-        # elif dartJacobian.shape[1] == self.Dof:
-        #     J = dartJacobian
-        # else:
-        #     raise ValueError("Dimension of Jacobian seems wrong.")
+            # # (
+            # #     np.linalg.inv(self.skel.getBodyNode(0).getWorldTransform().rotation())
+            # #     @ self.skel.getBodyNode(n).getWorldJacobian(np.array([0, 0, 0]))[3:6, :]
+            # # )
+            # if dartJacobian.shape[1] < self.Dof:
+            #     #     J = np.pad(
+            #     #         dartJacobian,
+            #     #         ((0, 0), (0, self.Dof - dartJacobian.shape[1] % self.Dof)),
+            #     #         "constant",
+            #     #     )
+            #     indexPointer = 0
+            #     paddedJacobian = np.zeros((3, self.skel.getNumDofs()))
+            #     for i in range(0, self.skel.getNumDofs()):
+            #         if self.skel.getBodyNode(n).dependsOn(i):
+            #             paddedJacobian[:, i] = dartJacobian[:, indexPointer]
+            #             indexPointer += 1
+            #     J = paddedJacobian
+            # elif dartJacobian.shape[1] == self.Dof:
+            #     J = dartJacobian
+            # else:
+            #     raise ValueError("Dimension of Jacobian seems wrong.")
 
-        # return self.skel.getWorldJacobian(self.skel.getBodyNode(n))[3:, :]
+            # return self.skel.getWorldJacobian(self.skel.getBodyNode(n))[3:, :]
 
-        # Darts' world jacobian seems wrong for free floating base (translational degrees of freedom are not identity matrix)
-        return np.linalg.inv(
-            self.skel.getBodyNode(0).getTransform().rotation()
-        ) @ self.skel.getLinearJacobian(self.skel.getBodyNode(n))
-        # jacobian = self.skel.getLinearJacobian(self.skel.getBodyNode(n))
-        # jacobian[:, :3] = np.eye(3)
-        # return self.skel.getLinearJacobian(self.skel.getBodyNode(n))
+            # Darts' world jacobian seems wrong for free floating base (translational degrees of freedom are not identity matrix)
+            return np.linalg.inv(
+                self.skel.getBodyNode(0).getTransform().rotation()
+            ) @ self.skel.getLinearJacobian(self.skel.getBodyNode(n))
+            # jacobian = self.skel.getLinearJacobian(self.skel.getBodyNode(n))
+            # jacobian[:, :3] = np.eye(3)
+            # return self.skel.getLinearJacobian(self.skel.getBodyNode(n))
+            # Compute adjoint of transform from body to world
+
+        elif method == "world":
+            if n == 1:
+                joint_offset = - self.skel.getBodyNode(n).getChildJoint(0).getTransformFromParentBodyNode().translation()
+            else:
+                joint_offset = self.skel.getBodyNode(n).getParentJoint().getTransformFromChildBodyNode().translation()
+
+            return self.skel.getWorldJacobian(self.skel.getBodyNode(n),joint_offset)[3:,:]
+        elif method == "first_body":
+            return self.skel.getJacobian(self.skel.getBodyNode(n),self.skel.getBodyNode(0))[3:,:]
+        elif method == "adjoint":
+            raise NotImplementedError
+            self.skel.setPositions(q)
+            # Get the body node's transform in world coordinates
+            T = self.skel.getBodyNode(n).getWorldTransform()
+
+            joint_offset = self.skel.getBodyNode(n).getChildJoint(0).getTransformFromParentBodyNode().translation()
+
+            # Get the spatial Jacobian in the body frame (or local frame at offset)
+            J_body = self.skel.getBodyNode(n).getJacobian(-joint_offset)
+
+            # Compute adjoint of transform from body to world
+            R = T.rotation()
+            p = T.translation()
+            px = self.skew_symmetric(p)
+            Ad_T = np.block([
+                [R, np.zeros((3, 3))],
+                [px @ R, R]
+            ])
+
+            # Transform to world frame using adjoint
+            J_world = Ad_T @ J_body
+            return J_world[3:, :]  # Return only the linear part
 
     def dependsOn(self, bodyNodeIndex, genCoordinateIndex):
         return self.skel.getBodyNode(bodyNodeIndex).dependsOn(genCoordinateIndex)
+    
+    def skew_symmetric(self,v):
+        """Create a 3x3 skew-symmetric matrix from a 3D vector."""
+        screw_symmetric_matrix = np.array([
+            [0,     -v[2],  v[1]],
+            [v[2],   0,    -v[0]],
+            [-v[1],  v[0],  0]
+        ])
+        return screw_symmetric_matrix

src/tracking/kpr/kpr.py
@@ -504,12 +504,13 @@ class KinematicsPreservingRegistration(NonRigidRegistration):
             P1_norm = self.P1 / np.mean(self.P1)
             # P1_norm[3:6] = np.ones(3)
             Wp = np.diag(np.repeat(1 / (1 + (np.exp(-(P1_norm)))), self.D))
+            # Wp = np.eye(Wp.shape[0])
             for iter in range(0, self.ik_iterations):
                 error = X_target - self.T
                 # assembly Jacobian
                 Jn_list = []
                 for n in range(0, self.N):
-                    Jn = self.model.getJacobian(self.q, n)
+                    Jn = self.model.getJacobian(self.q, n, method="first_body")
                     Jn_list.append(Jn)
                 J = np.vstack(Jn_list)
                 # pInv = dampedPseudoInverse(J, jacobianDamping)
@@ -534,10 +535,21 @@ class KinematicsPreservingRegistration(NonRigidRegistration):
                 # pInv = dampedPseudoInverse(A, jacobianDamping)
                 dq_stiff = q_0 - self.q
                 dq_stiff[0:6] = np.zeros(6)
-                self.dq = np.linalg.inv(A) @ (
-                    J.T @ Wp.T @ Wp @ error.flatten()
-                    + wStiffness * stiffnessMatrix @ (dq_stiff)
-                )
+                stiffnessMatrix[0:6,0:6] = np.zeros((6,6))
+                # t_start_solve_q_inverse = time.time()
+                # self.dq = np.linalg.inv(A) @ (
+                #     J.T @ Wp.T @ Wp @ error.flatten()
+                #     + wStiffness * stiffnessMatrix @ (dq_stiff)
+                # )
+                # t_end_solve_q_inverse = time.time()
+                # print("Time for inverse: {}".format(t_end_solve_q_inverse - t_start_solve_q_inverse))
+
+                # t_start_solve_q_linsys = time.time()
+                rhs = (
+                    J.T @ Wp.T @ Wp @ error.flatten()+ wStiffness * stiffnessMatrix @ dq_stiff)
+                self.dq = np.linalg.solve(A, rhs)
+                # t_end_solve_q_linsys = time.time()
+                # print("Time for linear system: {}".format(t_end_solve_q_linsys-t_start_solve_q_linsys))
                 # --------------------------------------------------------------
                 # self.dq[0:3] = self.dq[9:12]
                 # self.dq[6:9] = self.dq[9:12]